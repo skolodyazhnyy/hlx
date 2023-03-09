0e4282c5e018d5bb98fb475a03484acb2e892395
Sergey Kolodyazhnyy <kolodyaz@adobe.com>
2023-03-09 14:59:56 +0100

    Flightpicker block

---
+++ blocks/flight-picker/flight-picker.js
@@ -0,0 +1,11 @@
+/**
+ * loads and decorates the footer
+ * @param {Element} block The header block element
+ */
+
+// eslint-disable-next-line import/no-unresolved
+import 'https://343284-flightpickermfe-stage.adobeio-static.net/index.a92eb863.js';
+
+// eslint-disable-next-line no-unused-vars
+export default async function decorate(block) {
+}
