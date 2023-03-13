f09ca555faa9e5af0bb7d9911cbda897045b6f77
Sergey Kolodyazhnyy <kolodyaz@adobe.com>
2023-03-13 15:33:12 +0100

    Revert changes to links to fligh picker

--- blocks/flight-picker/flight-picker.js
+++ blocks/flight-picker/flight-picker.js
@@ -4,7 +4,7 @@
  */
 
 // eslint-disable-next-line import/no-unresolved
-import 'https://343284-flightpickermfe-stage.adobeio-static.net/flight-picker.js';
+import 'https://343284-flightpickermfe-stage.adobeio-static.net/index.a92eb863.js';
 
 // eslint-disable-next-line no-unused-vars
 export default async function decorate(block) {
