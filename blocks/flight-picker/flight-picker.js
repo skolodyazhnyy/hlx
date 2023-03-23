87ae70a8feb5e4ec25ce36fe76f2b9490d875cde
Sergey Kolodyazhnyy <kolodyaz@adobe.com>
2023-03-23 15:06:21 +0100

    Trying minimized build

--- blocks/flight-picker/flight-picker.js
+++ blocks/flight-picker/flight-picker.js
@@ -2,15 +2,15 @@
  * loads and decorates the footer
  * @param {Element} block The header block element
  */
-import { loadCSS } from '../../scripts/lib-franklin.js';
+// import { loadCSS } from '../../scripts/lib-franklin.js';
 
 // eslint-disable-next-line import/no-unresolved
 export default async function decorate(block) {
   const picker = document.createElement('script');
   picker.setAttribute('defer', 'true');
-  picker.setAttribute('src', 'https://343284-flightpickermfe-stage.adobeio-static.net/index.a92eb863.js');
+  picker.setAttribute('src', 'https://static.eolymp.com/adobecdn/main.js');
 
   document.querySelector('head').appendChild(picker);
 
-  loadCSS('https://343284-flightpickermfe-stage.adobeio-static.net/index.274a6c21.css');
+  // loadCSS('https://343284-flightpickermfe-stage.adobeio-static.net/index.274a6c21.css');
 }
