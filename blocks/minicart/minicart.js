b2fed3882bd9aa6422b616edb88273427a9de926
Sergey Kolodyazhnyy <kolodyaz@adobe.com>
2023-04-13 12:28:31 +0200

    Move minicart code to extrnal CDN

--- blocks/minicart/minicart.js
+++ blocks/minicart/minicart.js
@@ -3,9 +3,13 @@
  * @param {Element} block The header block element
  */
 
-import '../../scripts/hlx-minicart/hlx-minicart.js';
-
 export default async function decorate(block) {
   const component = document.createElement('hlx-minicart');
   block.append(component);
+
+  const picker = document.createElement('script');
+  picker.setAttribute('defer', 'true');
+  picker.setAttribute('src', 'https://static.eolymp.com/adobecdn/hlx-minicart.js');
+
+  document.querySelector('head').appendChild(picker);
 }
